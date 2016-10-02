70e455be61003a43d6de413b86934fd8338b33da
waps12b <waps12b@naver.com>
2016-10-03 03:51:32 +0900

    Initial Commit

---
+++ real-recognizer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  real-recognizer
+//
+//  Created by 김동이 on 2016. 10. 3..
+//  Copyright © 2016년 김동이. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
